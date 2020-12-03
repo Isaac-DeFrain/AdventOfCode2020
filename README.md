5ee6470b9d4ff9ae35b38aa9d0b12db0534d681b
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-12-03 09:37:19 -0700

    Part 2 solution

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-
-
-
-
 # puzzle1
 
-This is the description
-of the puzzle1 OCaml project
-
-
-* Website: Not yet specified
-* General Documentation: Not yet specified
-* API Documentation: Not yet specified
-* Sources: Not yet specified
+This is the first puzzle of Advent of Code 2020
--- src/puzzle1/main.ml
+++ src/puzzle1/main.ml
@@ -33,42 +33,83 @@ let parse_with_error lexbuf =
       fprintf stderr "%a: syntax error\n" print_position lexbuf ;
       exit (-1)
 
-let find_elements data =
+let find_two_elements data =
   List.map ~f:(fun x ->
     List.map ~f:(fun y -> y, x, x + y) data) data
   |> List.filter ~f:(fun l -> List.length l > 0)
 
-let rec print_ans strs = function
+let find_three_elements data =
+  List.map ~f:(fun x ->
+    List.map ~f:(fun y -> y, x, x + y) data) data
+  |> List.concat
+  |> List.map ~f:(fun (x, y, z) ->
+      List.map ~f:(fun w -> w, x, y, w + z) data)
+  |> List.filter ~f:(fun l -> List.length l > 0)
+
+let rec print_two_ans strs = function
 | [] -> if Poly.(<>) strs "" then print_endline strs
 | (i,j,k)::tl ->
-    if k <> 2020 then print_ans strs tl
+    if k <> 2020 then print_two_ans strs tl
     else
     let i' = string_of_int i in
     let j' = string_of_int j in
     let sum = string_of_int (i + j) in
     let prod = string_of_int (i * j) in
-    print_ans (String.concat ~sep:" "
+    print_two_ans (String.concat ~sep:" "
       [i'; "+"; j'; "="; sum; "&&"; i'; "*"; j'; "="; prod]) tl
 
-let print_ans' = print_ans ""
+let rec print_three_ans strs = function
+| [] -> if Poly.(<>) strs "" then print_endline strs
+| (i,j,k,l)::tl ->
+    if l <> 2020 then print_three_ans strs tl
+    else
+    let i' = string_of_int i in
+    let j' = string_of_int j in
+    let k' = string_of_int k in
+    let sum = string_of_int (i + j + k) in
+    let prod = string_of_int (i * j * k) in
+    print_three_ans (String.concat ~sep:" "
+      [i'; "+"; j'; "+"; k'; "="; sum; "&&"; i'; "*"; j'; "*"; k'; "="; prod]) tl
+
+let print_two_ans' = print_two_ans ""
+
+let print_three_ans' = print_three_ans ""
 
 let print_elems d data =
   if d then List.iter ~f:(Printf.printf "%d\n") data
 
-let print_product res data =
-  if res then find_elements data |> List.iter ~f:print_ans'
+let print_two_product two data =
+  if two then find_two_elements data |> List.iter ~f:print_two_ans'
+
+let print_three_product three data =
+  if three then find_three_elements data |> List.iter ~f:print_three_ans'
 
 (* handler function *)
-let handler f d res () =
+let handler f d two three () =
   let open In_channel in
-  let handle d res data =
-    match d, res with
-    | true, true ->
+  let handle d two three data =
+    match d, two, three with
+    | true, true, true ->
+        print_endline "-----original-----" ;
         print_elems d data ;
-        print_endline "--------------------" ;
-        print_product res data
-    | true, _ -> print_elems d data
-    | false, true -> print_product res data
+        print_endline "-----two-nums-----" ;
+        print_two_product two data ;
+        print_endline "-----three-nums-----" ;
+        print_three_product three data
+    | true, true, _ ->
+        print_endline "-----original-----" ;
+        print_elems d data ;
+        print_endline "-----two-nums-----" ;
+        print_two_product two data
+    | false, true, true ->
+        print_endline "-----two-nums-----" ;
+        print_two_product two data ;
+        print_endline "-----three-nums-----" ;
+        print_three_product three data
+    | false, true, _ ->
+        print_endline "-----two-nums-----" ;
+        print_two_product two data
+    | false, false, true -> print_three_product three data
     | _ -> ()
   in
   match f with
@@ -78,7 +119,7 @@ let handler f d res () =
       let lexbuf = Lexing.from_channel inx in
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename } ;
       let data = parse_with_error lexbuf in
-      handle d res data ;
+      handle d two three data ;
       close inx
 
 (* cli *)
@@ -89,6 +130,7 @@ let () =
       empty
       +> flag "-f" (optional string) ~doc:" Read given file"
       +> flag "-d" no_arg ~doc:" Display the num list"
-      +> flag "-2020" no_arg ~doc:" Display nums that add to 2020")
+      +> flag "-2" no_arg ~doc:" Display two nums that add to 2020"
+      +> flag "-3" no_arg ~doc:" Display three nums that add to 2020")
     handler
   |> Command.run
