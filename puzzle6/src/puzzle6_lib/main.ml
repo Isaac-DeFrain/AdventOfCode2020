bfe5514cb36ad796323230cdb32cf95c859d3458
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-12-06 17:39:49 -0700

    Puzzle 6 part 1 solution

--- puzzle6/src/puzzle6_lib/main.ml
+++ puzzle6/src/puzzle6_lib/main.ml
@@ -10,8 +10,55 @@
 (*                                                                        *)
 (**************************************************************************)
 
+let yeses = ref ([] : string list list)
 
-(* If you delete or rename this file, you should add
-   'src/puzzle6_lib/main.ml' to the 'skip' field in "drom.toml" *)
+let drop_while f = Core.List.drop_while ~f
 
-let main () = Printf.printf "Hello world!\n"
+let take_while f = Core.List.take_while ~f
+
+let non_empty = Core.Poly.(<>) ""
+
+let tail = List.tl
+
+let dedup_and_sort = Core.List.dedup_and_sort ~compare:String.compare
+
+(* exclusive range *)
+let ( -- ) a b =
+   let rec aux acc x y =
+      if x >= y then List.rev acc
+      else aux (x :: acc) (x + 1) y
+   in
+   aux [] a b
+
+let explode s =
+   let len = String.length s in
+   let chars = List.map (String.get s) (0 -- len) in
+   List.map Char.escaped chars
+
+let rec parse_file filename =
+   let open Core.In_channel in
+   let inx = create filename in
+   let ls = input_lines inx in
+   yeses := process ls ;
+   close inx
+
+and process ls =
+   groups ls |> List.map (List.map explode)
+   |> List.map List.concat |> List.map dedup_and_sort
+
+and groups = function
+   | [] -> []
+   | ls ->
+      let next = take_while non_empty ls in
+      let rem =
+         try drop_while non_empty ls |> tail with
+         | Failure _ -> []
+      in
+      next :: groups rem
+
+let solution_pt1 ys = List.map List.length ys |> List.fold_left ( + ) 0
+
+let main () =
+   Printf.printf "Puzzle 6:\n" ;
+   parse_file "test/puzzle6.input" ;
+   Printf.printf "Pt1 solution: %d\n" (solution_pt1 !yeses)
