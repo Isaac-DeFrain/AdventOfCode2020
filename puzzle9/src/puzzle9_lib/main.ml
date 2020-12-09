07371bbbeabe51ea8a9dea72837a2bea9c4d4cdd
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-12-09 14:03:57 -0700

    Puzzle 9 part 2 solution

--- puzzle9/src/puzzle9_lib/main.ml
+++ puzzle9/src/puzzle9_lib/main.ml
@@ -21,7 +21,7 @@ let rec parse_file filename =
 
 and process_nums = List.map int_of_string
 
-(* exclusive list slice *)
+(** exclusive list slice *)
 let rec slice m n l =
    if m < 0 || n <= m then []
    else match l with
@@ -49,8 +49,48 @@ let rec check_pos n =
    else if not (isValid n (nth !nums n)) then nth !nums n
    else check_pos (n + 1)
 
+(** exclusive range *)
+let ( -- ) a b =
+   let rec aux acc a b =
+      if b < a then List.rev acc
+      else aux (a :: acc) (a + 1) b
+   in
+   aux [] a b
+
+let contigs n l =
+   let open List in
+   let len = length l in
+   if n <= 0 then []
+   else if len <= n then [l]
+   else
+   let span = 0 -- (len - n) in
+   map (fun m -> slice m (m + n) l) span
+
+let sum = List.fold_left (+) 0
+
+exception Next
+
+let weakness m =
+   let inv = check_pos 0 in
+   let rec check = function
+   | [] -> raise Next
+   | hd::tl ->
+      if inv = sum hd then
+      let min_ = List.fold_left min (List.hd hd) hd in
+      let max_ = List.fold_left max (List.hd hd) hd in
+      min_ + max_
+      else check tl
+   in check (contigs m !nums)
+
+let rec find_weakness m =
+   if m <= List.length !nums then
+   try weakness m with
+   | Next -> find_weakness (m + 1)
+   else 0
+
 let main () =
    Printf.printf "~~~ Puzzle 9 ~~~\n" ;
    parse_file "test/puzzle9.input" ;
    Printf.printf "Pt1 solution: %d\n" (check_pos 0) ;
+   Printf.printf "Pt2 solution: %d\n" (find_weakness 2) ;
    ()
